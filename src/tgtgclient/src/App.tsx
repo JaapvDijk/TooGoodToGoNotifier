78ca06a14de5304c382ef2cd63c957f1db3a9e9f
Jaap van Dijk <dijkvanjaap@hotmail.com>
2024-04-11 13:01:07 +0200

    api client test

--- src/tgtgclient/src/App.tsx
+++ src/tgtgclient/src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Api } from './apiClient/Api';
+import { Basket } from './apiClient/data-contracts';
+
 
 function App() {
-  return (
+    const [data, setData] = useState<Basket[] | null>(null); //Any?
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const api = new Api({
+                baseUrl: "http://localhost:5000/api/v1",
+            });
+
+            try {
+                const res = await api.basketFavoriteList();
+                setData(res.data);
+                console.log(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -16,7 +39,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+                    Learn React
         </a>
       </header>
     </div>
